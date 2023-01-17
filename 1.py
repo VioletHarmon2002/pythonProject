d6abd565603b0487f8304b2ec0e783a9fba3ed4b
Елизавета <elizaveta@MacBook-Air-Elizaveta.local>
2023-01-17 19:53:53 +0200

    Initial commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+leviskät = float
---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+import math
+
+säde = float(input('Anna ympyrän säde: '))
+pintaAla = math.pi * säde * säde
+
+print(pintaAla)
+
+print("Hei,Liza!")
+user = input('Anna nimesi: ')
+print("Terve," + user + "!")
+
+
+
+
+
+
+
